c0d8ff6812a368b0d798016fca982ad45689c1e5
kodeartisan <kodeartisan@gmail.com>
2017-12-21 11:32:50 +0700

    remove unnecessary library

--- app/src/main/java/com/idkotlin/idreminder/presentation/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/idkotlin/idreminder/presentation/di/component/ApplicationComponent.kt
@@ -10,7 +10,6 @@ import com.tutorial.learnlinuxpro.data.source.local.room.AppDatabase
 import com.tutorial.learnlinuxpro.presentation.bus.RxBus
 import com.tutorial.learnlinuxpro.presentation.di.module.ApplicationModule
 import com.tutorial.learnlinuxpro.presentation.di.module.DataModule
-import com.tutorial.learnlinuxpro.presentation.di.module.NetworkModule
 import dagger.Component
 import javax.inject.Singleton
 
@@ -19,7 +18,7 @@ import javax.inject.Singleton
  */
 @Singleton
 @ApplicationScope
-@Component(modules = arrayOf(ApplicationModule::class, NetworkModule::class, DataModule::class))
+@Component(modules = arrayOf(ApplicationModule::class, DataModule::class))
 interface ApplicationComponent {
 
     fun getSchedulerProvider(): BaseSchedulerProvider
